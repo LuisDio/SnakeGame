2647bec9d2c8a2eeecacfb07ec6998ce5f579559
LuisDio <luis.diolindo@gmail.com>
2018-10-03 10:04:36 +0300

    creating block within the canvas

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 // create a canvas when window is loaded/open
 window.onload = function() {
 
-    // Create a canvas element with given property
-    var canvas = document.createElement('canvas');
-    canvas.width = 800;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    // Add this canvas element to our html body tag
-    document.body.appendChild(canvas);
+    function init() {
 
-    // create an object within the canvas
-    var rectObject = canvas.getContext('2d');
-    rectObject.fillStyle = "#ff0000";
-    rectObject.fillRect(30, 30, 100, 50);
+        // Create a canvas element with given property
+        var canvas = document.createElement('canvas');
+        canvas.width = 800;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        // Add this canvas element to our html body tag
+        document.body.appendChild(canvas);
+
+        // create an object within the canvas
+        var rectObject = canvas.getContext('2d');
+        rectObject.fillStyle = "#ff0000";
+        rectObject.fillRect(30, 30, 100, 50);
+
+
+    }
+
+    
 }
